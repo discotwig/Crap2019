4790c6c9f99fd0e4c27cb5735eb207295ab96133
discotwig <47826566+discotwig@users.noreply.github.com>
2019-10-22 17:53:41 -0400

    linked buy in to game file

--- Crap_v2/app/src/main/java/com/example/crap_v2/Start.java
+++ Crap_v2/app/src/main/java/com/example/crap_v2/Start.java
@@ -4,10 +4,19 @@ package com.example.crap_v2;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import org.w3c.dom.Text;
+
 public class Start extends AppCompatActivity {
+
+    EditText buy;
+    TextView warning;
+    int amount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +24,27 @@ public class Start extends AppCompatActivity {
     }
 
     public void MainStartClick(View v) {
+
+        buy = (EditText) findViewById(R.id.Enteramount);
+        warning = (TextView) findViewById(R.id.warning);
+
         switch (v.getId()) {
             case R.id.btnStartGame:
-                Intent start = new Intent(Start.this, Game.class);
-                startActivity(start);
-                break;
 
+                String input = buy.getText().toString();
+                amount = Integer.parseInt(input);
+
+                // if buy-in is not divisible by 5
+                if (amount % 5 != 0) {
+                    warning.setText(getString(R.string.warning));
+                    break;
+                }
+                else {
+                    Intent start = new Intent(Start.this, Game.class);
+                    start.putExtra("buyin", amount);
+                    startActivity(start);
+                    break;
+                }
             case R.id.btnInstruct:
                 Intent instruct = new Intent(Start.this, Instruct.class);
                 startActivity(instruct);
